7e475c351146dce70c9a00aa76349efc78f3edb1
Bhushan727 <bhushans727@gmail.com>
2023-01-02 14:42:43 +0530

    removed remaining errors

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -8,19 +8,17 @@ const MovieList = () => {
     const [movieList , setMovieList ] = useState([]);
     const { type } = useParams();
 
-    useEffect(() => {
-        getData();
-    },[])
+    // useEffect(() => {
+    //     fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=83bc1ac49da602b72f4a3140351d22a2`)
+    //     .then(res => res.json())
+    //     .then(data => setMovieList(data.results))
+    // },[])
 
     useEffect(() => {
-        getData();
-    },[type])
-
-    const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=83bc1ac49da602b72f4a3140351d22a2`)
         .then(res => res.json())
         .then(data => setMovieList(data.results))
-    }
+    },[type])
 
   return (
     <div className='movie__list'>
@@ -28,7 +26,7 @@ const MovieList = () => {
         <div className="list__cards">
             {
                 movieList.map(movie => (
-                    <Card movie={movie} />
+                    <Card movie={movie} key={movie.id}/>
                 ))
             }
         </div>
